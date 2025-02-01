3c92d566d085a793989fca39133dea178976c041
hridayadhikari <hriday.cse.tcea.2026@gmail.com>
2025-02-01 12:13:45 +0530

    Add website files

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+}
+
+const images = [
+    './assets/img1.jpg',
+    './assets/img2.jpg',
+    './assets/img3.jpg',
+];
+
+let currentIndex = 0;
+
+function changeBackground() {
+    document.querySelector(".slideshow-container").style.backgroundImage = `url('${images[currentIndex]}')`;
+    currentIndex = (currentIndex + 1) % images.length;
+}
+
+setInterval(changeBackground, 2000); // Change every 5 seconds
+changeBackground(); // Initial call
+
